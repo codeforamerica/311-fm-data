b9f980b87f50bc85bedff1f9e6dbe4715f692f55
Jesse Bounds <jesse@rebounds.net>
2012-09-11 17:38:57 -0700

    added all wards summary api call and database proc to back it

--- api_server.js
+++ api_server.js
@@ -15,6 +15,59 @@ app.get('/', function(req, res){
   res.send("311.fm API");
 });
 
+
+app.get('/api/v1/ward/summary', function(req, res){
+
+  console.log(req.originalUrl);
+
+  start = req.query.start;
+  end = req.query.end;
+
+  if (null === start || undefined === start || '' === start) {
+    res.send(406, "start and end date required");
+  }
+  if (null === end || undefined === end || '' === end) {
+    res.send(406, "start and end date required");
+  }
+
+  responses = [];
+
+  response = {
+    type: "Summary",
+    ward: "All",
+    summaries: responses
+  };
+
+  database.query(
+    "SELECT opened_requests, " +
+    "       closed_requests, " +
+    "       tardy_requests, " +
+    "       days_to_close_requests_avg, " +
+    "       request_time_bins, " +
+    "       request_counts, " +
+    "       ward " +
+    "FROM ward_summary('" + start +"', '" + end + "') " +
+    "AS " +
+    "(opened_requests int, " +
+    " closed_requests int, " +
+    " tardy_requests int, " +
+    " days_to_close_requests_avg double precision, " +
+    " request_time_bins text, " +
+    " request_counts text, " +
+    " ward text)"
+  ).on('row', function(result) {    
+    result.request_time_bins = JSON.parse(result.request_time_bins);
+    result.request_counts = JSON.parse(result.request_counts);
+    response.stats = result;
+    responses.push(result);
+  }).on('end', function() {
+    console.log(response);
+    res.jsonp(200, response);
+  });
+
+});
+
+
 app.get('/api/v1/:ward/summary', function(req, res){
 
   console.log(req.originalUrl);
---
+++ database/create_sp_ward_all_summary.sql
@@ -0,0 +1,39 @@
+﻿-- 311.fm SUMMARY STORED PROC
+-- CALCULATE SUMMARY DATA FOR WARD IN A TIME RANGE
+-- AUTHOR: JESSE BOUNDS
+-- DATE: 9/11/2012
+
+CREATE FUNCTION ward_summary(start_date timestamp, end_date timestamp)
+RETURNS setof RECORD AS $$
+
+  DECLARE
+  rec RECORD;
+
+  BEGIN
+
+  FOR i IN 1..50 LOOP -- OUTER
+
+  FOR rec in SELECT opened_requests, 
+                  closed_requests, 
+                  tardy_requests,
+                  days_to_close_requests_avg,
+                  request_time_bins,
+                  request_counts,
+                  trim(to_char(i, '99')) as ward
+           FROM ward_summary(trim(to_char(i, '99')), start_date, end_date)
+           AS (opened_requests int,
+               closed_requests int,
+               tardy_requests int,
+               days_to_close_requests_avg double precision,
+               request_time_bins text,
+               request_counts text)
+  LOOP -- INNER
+    RETURN NEXT rec;
+  END LOOP; -- INNER
+
+  END LOOP; -- OUTER
+  
+  RETURN;
+
+  END;
+$$ LANGUAGE 'plpgsql'
